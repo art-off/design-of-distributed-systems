13853a62e707519e38dc754372f8b473d6dd023c
Artem Rylov <tema2707@icloud.com>
2023-09-14 19:18:11 +0700

    sleep server

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,7 +1,7 @@
 import { createServer, Socket } from "net";
 
 import { parseLogin, parseMathExpr } from "../parsing";
-import { mathExprToString } from "../utils";
+import { mathExprToString, sleep } from "../utils";
 import { calculateMathExpr, log } from "./utils";
 
 // Configuration parameters
@@ -18,26 +18,29 @@ const checkLogin = (login: string) => {
     }
 }
 
+const handleClientData = async (sock: Socket, data: Buffer) => {
+    await sleep(1);
+    const stringMessage = data.toString();
+    try {
+        const login = parseLogin(stringMessage);
+        checkLogin(login);
+
+        const expr = parseMathExpr(stringMessage);
+        log(sock, `Received: ${mathExprToString(expr)}`);
+
+        const result = calculateMathExpr(expr);
+        log(sock, `Calculated: ${result}`);
+
+        sock.write(result.toString())
+    } catch (e) {
+        let message = `Error: ${e.message}`
+        log(sock, message);
+        sock.write(message);
+    }
+}
+
 const handleClinetConnection = (sock: Socket) => {
-    sock.on('data', (data) => {
-        const stringMessage = data.toString();
-        try {
-            const login = parseLogin(stringMessage);
-            checkLogin(login);
-
-            const expr = parseMathExpr(stringMessage);
-            log(sock, `Received: ${mathExprToString(expr)}`);
-
-            const result = calculateMathExpr(expr);
-            log(sock, `Calculated: ${result}`);
-
-            sock.write(result.toString())
-        } catch (e) {
-            let message = `Error: ${e.message}`
-            log(sock, message);
-            sock.write(message);
-        }
-    });
+    sock.on('data', (data) => { handleClientData(sock, data); });
 }
 
 const server = createServer(handleClinetConnection);
